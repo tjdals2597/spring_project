379f6ea5a82730aa17e965e424c042f79fabfd15
tjdals2597 <tjdals2597@naver.com>
2024-07-26 11:47:44 +0900

    web_controller edit

--- shopping_mall/src/main/java/com/bagshop/www/web_controller.java
+++ shopping_mall/src/main/java/com/bagshop/www/web_controller.java
@@ -3,9 +3,15 @@ package com.bagshop.www;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_controller {
 
 	PrintWriter pw = null;
+	
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		return null;
+	}
 }
